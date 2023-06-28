5b78503660a3c46756b3a2f21e0a186e90235c8f
Safak Turkeli <sfktrkl@gmail.com>
2023-06-28 22:20:07 +0300

    Recyclable and Low Fat Products

---
+++ Select/Recyclable and Low Fat Products/schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS Products (
+    product_id INT,
+    low_fats ENUM('Y', 'N'),
+    recyclable ENUM('Y', 'N')
+);
+TRUNCATE TABLE Products;
+INSERT INTO Products (product_id, low_fats, recyclable)
+VALUES ('0', 'Y', 'N');
+INSERT INTO Products (product_id, low_fats, recyclable)
+VALUES ('1', 'Y', 'Y');
+INSERT INTO Products (product_id, low_fats, recyclable)
+VALUES ('2', 'N', 'Y');
+INSERT INTO Products (product_id, low_fats, recyclable)
+VALUES ('3', 'Y', 'Y');
+INSERT INTO Products (product_id, low_fats, recyclable)
+VALUES ('4', 'N', 'N');
