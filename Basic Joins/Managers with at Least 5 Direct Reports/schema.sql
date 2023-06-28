ba77dd0aa3092b44e50c730c317625c50a231b44
Safak Turkeli <sfktrkl@gmail.com>
2023-06-29 00:58:09 +0300

    Managers with at Least 5 Direct Reports

---
+++ Basic Joins/Managers with at Least 5 Direct Reports/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS Employee (
+    id INT,
+    name VARCHAR(255),
+    department VARCHAR(255),
+    managerId INT
+);
+TRUNCATE TABLE Employee;
+INSERT INTO Employee (id, name, department, managerId)
+VALUES (101, 'John', 'A', NULL);
+INSERT INTO Employee (id, name, department, managerId)
+VALUES (102, 'Dan', 'A', 101);
+INSERT INTO Employee (id, name, department, managerId)
+VALUES (103, 'James', 'A', 101);
+INSERT INTO Employee (id, name, department, managerId)
+VALUES (104, 'Amy', 'A', 101);
+INSERT INTO Employee (id, name, department, managerId)
+VALUES (105, 'Anne', 'A', 101);
+INSERT INTO Employee (id, name, department, managerId)
+VALUES (106, 'Ron', 'B', 101);
