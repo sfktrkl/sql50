335f0a5a0a818c794c6badec3472237b8c300ba6
Safak Turkeli <sfktrkl@gmail.com>
2023-06-28 22:34:57 +0300

    Invalid Tweets

---
+++ Select/Invalid Tweets/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS Tweets (tweet_id INT, content VARCHAR(50));
+TRUNCATE TABLE Tweets;
+INSERT INTO Tweets (tweet_id, content)
+VALUES (1, 'Vote for Biden');
+INSERT INTO Tweets (tweet_id, content)
+VALUES (2, 'Let us make America great again!');
