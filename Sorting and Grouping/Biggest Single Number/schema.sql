a336a92d63ab7571bd40221b0424e7904a5bd6e8
Safak Turkeli <sfktrkl@gmail.com>
2023-06-30 22:37:30 +0300

    Biggest Single Number

---
+++ Sorting and Grouping/Biggest Single Number/schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS MyNumbers (num INT);
+TRUNCATE TABLE MyNumbers;
+INSERT INTO MyNumbers (num)
+VALUES ('8');
+INSERT INTO MyNumbers (num)
+VALUES ('8');
+INSERT INTO MyNumbers (num)
+VALUES ('3');
+INSERT INTO MyNumbers (num)
+VALUES ('3');
+INSERT INTO MyNumbers (num)
+VALUES ('1');
+INSERT INTO MyNumbers (num)
+VALUES ('4');
+INSERT INTO MyNumbers (num)
+VALUES ('5');
+INSERT INTO MyNumbers (num)
+VALUES ('6');
