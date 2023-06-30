92539a7e0b634fcba4a4a255b25cf2b981f679bd
Safak Turkeli <sfktrkl@gmail.com>
2023-07-01 00:13:02 +0300

    Triangle Judgement

---
+++ Advanced Select and Joins/Triangle Judgement/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS Triangle (x INT, y INT, z INT);
+TRUNCATE TABLE Triangle;
+INSERT INTO Triangle (x, y, z)
+VALUES ('13', '15', '30');
+INSERT INTO Triangle (x, y, z)
+VALUES ('10', '20', '15');
---
+++ Advanced Select and Joins/Triangle Judgement/solution.sql
@@ -0,0 +1,10 @@
+SELECT x,
+    y,
+    z,
+    CASE
+        WHEN x + y <= z THEN 'No'
+        WHEN x + z <= y THEN 'No'
+        WHEN y + z <= x THEN 'No'
+        ELSE 'Yes'
+    END as triangle
+FROM Triangle
