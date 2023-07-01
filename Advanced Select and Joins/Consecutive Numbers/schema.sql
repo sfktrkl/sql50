8c27506cd1825e9dd17a8a489e7b759370f0546c
Safak Turkeli <sfktrkl@gmail.com>
2023-07-01 21:49:46 +0300

    Consecutive Numbers

---
+++ Advanced Select and Joins/Consecutive Numbers/schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS Logs (id INT, num INT);
+TRUNCATE TABLE Logs;
+INSERT INTO Logs (id, num)
+VALUES ('1', '1');
+INSERT INTO Logs (id, num)
+VALUES ('2', '1');
+INSERT INTO Logs (id, num)
+VALUES ('3', '1');
+INSERT INTO Logs (id, num)
+VALUES ('4', '2');
+INSERT INTO Logs (id, num)
+VALUES ('5', '1');
+INSERT INTO Logs (id, num)
+VALUES ('6', '2');
+INSERT INTO Logs (id, num)
+VALUES ('7', '2');
