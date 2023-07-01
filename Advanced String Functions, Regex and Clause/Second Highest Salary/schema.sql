a0b8189313ba8445cb6fdf51550cd002484e7f26
Safak Turkeli <sfktrkl@gmail.com>
2023-07-01 23:58:26 +0300

    Second Highest Salary

---
+++ Advanced String Functions, Regex and Clause/Second Highest Salary/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS Employee (id INT, salary INT);
+TRUNCATE TABLE Employee;
+INSERT INTO Employee (id, salary)
+VALUES ('1', '100');
+INSERT INTO Employee (id, salary)
+VALUES ('2', '200');
+INSERT INTO Employee (id, salary)
+VALUES ('3', '300');
