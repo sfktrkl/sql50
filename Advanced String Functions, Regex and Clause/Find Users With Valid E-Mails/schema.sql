8e31bd8350013cda9e5748cb915f7b7c3136c8b9
Safak Turkeli <sfktrkl@gmail.com>
2023-07-02 04:22:21 +0300

    Find Users With Valid E-Mails

---
+++ Advanced String Functions, Regex and Clause/Find Users With Valid E-Mails/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS Users (
+    user_id INT,
+    name VARCHAR(30),
+    mail VARCHAR(50)
+);
+TRUNCATE TABLE Users;
+INSERT INTO Users (user_id, name, mail)
+VALUES ('1', 'Winston', 'winston@leetcode.com');
+INSERT INTO Users (user_id, name, mail)
+VALUES ('2', 'Jonathan', 'jonathanisgreat');
+INSERT INTO Users (user_id, name, mail)
+VALUES ('3', 'Annabelle', 'bella-@leetcode.com');
+INSERT INTO Users (user_id, name, mail)
+VALUES ('4', 'Sally', 'sally.come@leetcode.com');
+INSERT INTO Users (user_id, name, mail)
+VALUES ('5', 'Marwan', 'quarz#2020@leetcode.com');
+INSERT INTO Users (user_id, name, mail)
+VALUES ('6', 'David', 'david69@gmail.com');
+INSERT INTO Users (user_id, name, mail)
+VALUES ('7', 'Shapiro', '.shapo@leetcode.com');
